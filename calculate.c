f109624f936c713b67f9da761442e0797389c183
aryana <aryana.zallnezhad@gmail.com>
2023-12-05 15:00:15 +0330

    has been commited7

--- calculate.c
+++ calculate.c
@@ -1,5 +1,8 @@
 #include <stdio.h> 
+int pow2(int a) {
+     return a * a;
+ }
 int calculate(int a, int b) {
     // A combination of both calculations
      return a * b * 2 -10 + (a + b); 
-  }
+}
